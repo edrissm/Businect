d2c3c27b900e640f57bf3407d3d072b183470bc4
edrissm <48758822+edrissm@users.noreply.github.com>
2019-04-30 16:27:12 +0200

    Add files via upload

---
+++ Businect/Businect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Businect
+//
+//  Created by Edriss Mosafer on 29.04.19.
+//  Copyright © 2019 Scrum-Made. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
