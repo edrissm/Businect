06c0c85ae179124eee63f1d3f3326dca8dbc7e53
Edriss <edriss.mosafer@gmail.com>
2019-04-30 16:14:34 +0200

    First Commit

---
+++ Businect-SM/Businect-SM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Businect-SM
+//
+//  Created by Edriss Mosafer on 30.04.19.
+//  Copyright © 2019 Scrum-Made. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
