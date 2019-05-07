402d15106c433eb1591d47284b9dbc8f0d08b28a
Muqarab <muqarabafzal@yahoo.com>
2019-05-07 16:47:00 +0200

    Daten werden zu der Firebase Datenbank übertragen

---
+++ Businect/Businect/RegisterViewController.swift
@@ -0,0 +1,67 @@
+//
+//  RegisterViewController.swift
+//  Businect
+//
+//  Created by Muqarab Afzal on 07.05.19.
+//  Copyright © 2019 Scrum-Made. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class RegisterViewController: UIViewController {
+    var refName: DatabaseReference!
+    @IBOutlet weak var textFieldName: UITextField!
+    
+    @IBOutlet weak var textFieldVorname: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPasswort: UITextField!
+    
+    @IBOutlet weak var textFieldBeruf: UITextField!
+    @IBOutlet weak var textFielBranche: UITextField!
+    @IBOutlet weak var textFieldInteresse1: UITextField!
+    
+    @IBOutlet weak var textFieldInteresse2: UITextField!
+    @IBOutlet weak var labelmessage: UILabel!
+    @IBAction func buttonAddUser(_ sender: UIButton) {
+        addName()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        FirebaseApp.configure()
+        
+        refName = Database.database().reference().child("Name")
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func addName(){
+        let key = refName.childByAutoId().key
+        
+        let name = ["id":key,
+                    "Name": textFieldName.text! as String,
+                    "Vorname": textFieldName.text! as String,
+                    "EMail": textFieldVorname.text! as String,
+                    "Passwort": textFieldPasswort.text! as String,
+                    "Branche": textFielBranche.text! as String,
+                    "Beruf": textFieldBeruf.text! as String,
+                    "Interesse1": textFieldInteresse1.text! as String,
+                    "Interesse2": textFieldInteresse2.text! as String
+        ]
+        
+        refName.child(key!).setValue(name)
+        
+        labelmessage.text = "Super"
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
