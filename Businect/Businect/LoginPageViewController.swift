035b4e6163312fa2d09c7be07695ce03b354c433
Edriss <edriss.mosafer@gmail.com>
2019-05-14 15:56:37 +0200

    Login

---
+++ Businect/Businect/LoginPageViewController.swift
@@ -0,0 +1,58 @@
+//
+//  LoginPageViewController.swift
+//  Businect
+//
+//  Created by Edriss Mosafer on 14.05.19.
+//  Copyright © 2019 Scrum-Made. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginPageViewController: UIViewController {
+
+  
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginClickButton(_ sender: Any) {
+        print("Login Button clicked")
+        
+        if(usernameTextField.text != "" && passwordTextField.text != ""){
+            Auth.auth().signIn(withEmail: usernameTextField.text!, password: passwordTextField.text!){
+                (user, error) in
+                
+              if(user != nil){
+                    print("user authenticated")
+                //let vc = self.storyboard?.instantiateViewController:"vcid"; as! ProfilseiteViewController
+                //self.present(vc, animated: true, completion: nil);
+                } else{
+                    print("there was an error")
+                    self.errorLabel.isHidden = false
+               }
+            }
+        } else {
+            print("there was an error")
+            self.errorLabel.isHidden = false
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
---
+++ Businect/Businect/ProfilseiteViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfilseiteViewController.swift
+//  Businect
+//
+//  Created by Edriss Mosafer on 14.05.19.
+//  Copyright © 2019 Scrum-Made. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ProfilseiteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutButtonClicked(_ sender: Any) {
+        do{
+        try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
