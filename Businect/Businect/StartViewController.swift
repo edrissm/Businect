b1db645969207a97af4419d1cbc349e91cf33129
Nina <n.erlacher@web.de>
2019-05-23 12:19:32 +0200

    code anschaulicher

--- Businect/Businect/StartViewController.swift
+++ Businect/Businect/StartViewController.swift
@@ -48,16 +48,6 @@ class StartViewController: UIViewController {
             } catch let signOutError as NSError {
                 print ("Error signing out: %@", signOutError)
             }
+        }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
 }
