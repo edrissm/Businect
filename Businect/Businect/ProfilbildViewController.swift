e19d4df550a1959c169cacc8cccc50425fa8be11
Nina <n.erlacher@web.de>
2019-06-06 11:10:37 +0200

    Variablen Umbenennung auf ProfielseiteViewController

--- Businect/Businect/ProfilbildViewController.swift
+++ Businect/Businect/ProfilbildViewController.swift
@@ -18,14 +18,14 @@ class ProfilbildViewController: UIViewController, UIImagePickerControllerDelegat
     @IBOutlet weak var imgImage: UIImageView!   //Wo ausgewählte Profilfoto angezeigt wird
     
     var imagePicker = UIImagePickerController()
-    @IBOutlet weak var FotoAuswählenEnablen: UIButton!
-    @IBOutlet weak var RegistrierungAbschließenEnablen: UIButton!
+    @IBOutlet weak var pickImageEnable: UIButton!
+    @IBOutlet weak var finishRegistrationEnablen: UIButton!
     
     //Man kann erst die Seite verlassen, nachdem man ein Profilfoto aus der Galerie ausgewählt hat.
     override func viewDidLoad() {
         super.viewDidLoad()
-        FotoAuswählenEnablen.isEnabled = true
-        RegistrierungAbschließenEnablen.isEnabled = false
+        pickImageEnable.isEnabled = true
+        finishRegistrationEnablen.isEnabled = false
     }
     
     var imageReference: StorageReference {
@@ -33,16 +33,16 @@ class ProfilbildViewController: UIViewController, UIImagePickerControllerDelegat
     }
     
     //Wenn ein Foto in der Galerie ausgewählt wird, wird dieses in den UIIMageView eingefügt und dem Benutzer wird es ermöglicht auf "Weiter" zu klicken
-    @IBAction func btnGalleryTapped(_ sender: Any) {
+    @IBAction func galleryTapped(_ sender: Any) {
         imagePicker.sourceType = .photoLibrary
         imagePicker.delegate = self
         self.present(imagePicker, animated: true, completion: nil)
-        FotoAuswählenEnablen.isEnabled = false
-        RegistrierungAbschließenEnablen.isEnabled = true
+        pickImageEnable.isEnabled = false
+        finishRegistrationEnablen.isEnabled = true
     }
 
     //Das Foto, welches sich im UIImageView befindet, wird auf in das Firebase-Storage geladen.
-    @IBAction func uplouddb(_ sender: Any) {
+    @IBAction func uploadImgToFirebase(_ sender: Any) {
             guard let image = imgImage.image else { return }
             guard let imageData2 = image.pngData() else { return }
             var fotoName : String = ""
@@ -56,7 +56,6 @@ class ProfilbildViewController: UIViewController, UIImagePickerControllerDelegat
             }
             uploadTask.observe(.progress){ (snapshot) in
                 print(snapshot.progress ?? "NO MORE PROGRESS")
-                
             }
             uploadTask.resume()
     }
