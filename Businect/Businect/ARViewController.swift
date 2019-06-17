5fec117526bd197d2c2f13a33b3e7b1e80eb1ef0
Edriss <edriss.mosafer@gmail.com>
2019-06-17 10:18:28 +0200

    QR Code Scanner Code(auskommentiert)

--- Businect/Businect/ARViewController.swift
+++ Businect/Businect/ARViewController.swift
@@ -12,9 +12,10 @@ import ARKit
 import Firebase
 import FirebaseAuth
 import FirebaseDatabase
+import AVFoundation
 
 // ViewController, welche die Kamera öffnet und automatisch nach QR-Codes scannt und diese mit den Nutzerinformationen augmented anzeigt.
-class ARViewController: UIViewController, ARSCNViewDelegate {
+class ARViewController: UIViewController, ARSCNViewDelegate, AVCaptureMetadataOutputObjectsDelegate {
     
     @IBOutlet weak var sceneView: ARSCNView!
     
@@ -27,6 +28,9 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     var tempInteresse1 = ""
     var tempInteresse2 = ""
     var tempVerfügbarkeit = true
+    
+    //Videoanzeige der Kamera
+    var video = AVCaptureVideoPreviewLayer()
    
     // Zeigt die eigenen Benutzerinformationen augmented an.
     // Created by Nina, Edriss and Muqarab
@@ -92,6 +96,35 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
             }
         })
         sceneView.delegate = self
+        
+        /*
+        //Creating session
+        let session = AVCaptureSession()
+        
+        //Define capture device
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)!
+        
+        //add the capture device to our session
+        do{
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+        }
+        catch{
+            print("ERROR")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        //add the session
+        session.addOutput(output)
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        video = AVCaptureVideoPreviewLayer(session: session)
+        //set frame
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        //session starten
+        session.startRunning()
+        */
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -118,5 +151,21 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    // Falls ein QR Code erkannt wurde, wird eine Benachrichtigung mit dessen Inhalt ausgegeben als eine Benachrichtigung.
+    // Created by Edriss
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects != nil && metadataObjects.count != 0{
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
+                if object.type == AVMetadataObject.ObjectType.qr{
+                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Kopieren", style: .default, handler: { (nil) in UIPasteboard.general.string = object.stringValue}))
+                    
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
 }
